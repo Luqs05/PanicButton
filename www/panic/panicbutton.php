68bb5772d14c1d159f9da387c83913710e26fbb6
Luqmaan Hassim <luqmaanhassim1@gmail.com>
2019-04-11 10:57:06 +0200

    Update panicbutton.php

--- www/panic/panicbutton.php
+++ www/panic/panicbutton.php
@@ -1,10 +1,10 @@
 <?php 
 
 	session_start();
-	$host        = "host=41.185.93.71";
+	$host        = "host=127.0.0.1";
 	$port        = "port=5432";
 	$dbname      = "dbname=devgroup3";
-	$credentials = "user=postgres password=Raeesah1";
+	$credentials = "user=postgres password=password";
 	
 	$db = pg_connect("$host $port $dbname $credentials");
 	if($db){
