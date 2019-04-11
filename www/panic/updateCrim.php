395e595d32b54bc6be6a13978e8edb3aac7cf977
Luqmaan Hassim <luqmaanhassim1@gmail.com>
2019-04-11 10:54:10 +0200

    Update updateCrim.php

--- www/panic/updateCrim.php
+++ www/panic/updateCrim.php
@@ -1,10 +1,10 @@
 <?php 
 
 	session_start();
-	$host        = "host=41.185.93.71";
+	$host        = "host=127.0.0.1";
 	$port        = "port=5432";
 	$dbname      = "dbname=devgroup3";
-	$credentials = "user=postgres password=Raeesah1";
+	$credentials = "user=postgres password=password";
 	$db = pg_connect("$host $port $dbname $credentials");
 	if($db){
 		echo 'connected';
@@ -12,20 +12,4 @@
 		
 	$query2 = "UPDATE public.incident SET type = 'Criminal Activity' WHERE id = (SELECT max(id) from public.incident)";
 	$result = pg_query($query2);
-	
-		
-		
-		/*
-		if (!$result) {
-  		$errormessage = pg_last_error();
-		echo $query1;
-		exit();
-		}
-		
-		printf ("These values were inserted into the database - %s %s %s" $varlat, $varlng);
-		pg_close();
-
-		header("Location: index.html");
-		exit;*/
-	
-		?>
+?>
