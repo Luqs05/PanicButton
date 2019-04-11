8f42cddb9a3ab76ede52a7ad4ec5b63ef233a436
Luqmaan Hassim <luqmaanhassim1@gmail.com>
2019-04-11 11:01:14 +0200

    Update insert.php

--- www/admin/insert.php
+++ www/admin/insert.php
@@ -1,10 +1,10 @@
 <?php
 	session_start();
 	session_start();
-	$host        = "host=41.185.93.71";
+	$host        = "host=127.0.0.1";
 	$port        = "port=5432";
 	$dbname      = "dbname=devgroup3";
-	$credentials = "user=postgres password=Raeesah1";
+	$credentials = "user=postgres password=password";
 	$db = pg_connect("$host $port $dbname $credentials");
 
 	$varlat = pg_escape_string($_POST['lat']);
